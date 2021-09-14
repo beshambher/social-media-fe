0265139ad3349a11d52c5800390fb4124b0a4334
beshmabher <bishamberc@gmail.com>
2021-09-15 02:00:12 +0530

    Added global error handler to trigger reload in the event of chunk loading failure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { HttpClientModule } from '@angular/common/http';
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { APP_INITIALIZER, ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GlobalErrorHandler } from './core/handlers/GlobalErrorHandler';
 import { AppInitService, initializeApp } from './core/services/initializer/app-init.service';
 import { SharedModule } from './shared/shared.module';
 
@@ -18,7 +19,8 @@ import { SharedModule } from './shared/shared.module';
     SharedModule
   ],
   providers: [
-    { provide: APP_INITIALIZER, useFactory: initializeApp, deps: [AppInitService], multi: true}
+    { provide: APP_INITIALIZER, useFactory: initializeApp, deps: [AppInitService], multi: true },
+    { provide: ErrorHandler, useClass: GlobalErrorHandler }
   ],
   bootstrap: [AppComponent]
 })
