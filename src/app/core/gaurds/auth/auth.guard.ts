f732c23d98adb37b002880e8e634dc13e83cdcb1
besh <bishamberc@gmail.com>
2025-08-03 23:52:54 +0200

    Upgraded angular version from v15 to v16

--- src/app/core/gaurds/auth/auth.guard.ts
+++ src/app/core/gaurds/auth/auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, UrlTree } from '@angular/router';
+import { Router, UrlTree } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
   constructor(private authService: AuthService, private router: Router) { }
 
--- src/app/core/gaurds/guest/guest.guard.ts
+++ src/app/core/gaurds/guest/guest.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, UrlTree } from '@angular/router';
+import { Router, UrlTree } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GuestGuard implements CanActivate {
+export class GuestGuard  {
   constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(): boolean | UrlTree {
@@ -14,5 +14,4 @@ export class GuestGuard implements CanActivate {
     }
     return true;
   }
-
 }
